0c4f0615cbe9cfea2930d41dcacf33ace898d301
Alexandi <kravz2702@gmail.com>
2021-01-30 22:53:09 +0300

    Ended up to templates(index.html) and finished(no) views.py

---
+++ Chapter_2/my_app/hello/views.py
@@ -0,0 +1,10 @@
+from flask import render_template, request, Blueprint
+from my_app.hello.models import MESSAGES
+
+hello = Blueprint('hello', __name__)
+
+@hello.route('/')
+@hello.route('/hello')
+def hello_world():
+    user = request.args.get('user', 'Iron man')
+    return render_template('index.html', user = user)
